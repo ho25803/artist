342605d9193f3eb7c2203c4407b87f344c812228
ho25803 <ho25803@gmail.com>
2025-11-24 10:20:25 +0900

    12

---
+++ kmh/src/kmh_ch06/carExample.java
@@ -0,0 +1,16 @@
+package kmh_ch06;
+
+public class carExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car myCar = new Car("포르쉐");
+		car yourCar = new Car("벤츠");
+		
+		myCar.run();
+		yourCar.run();
+		
+
+	}
+
+}
